c24eff5d6deb5c6aba6a7ea08c71049f415de9f9
josimarminetefera <josimaminete@gmail.com>
2020-11-08 16:17:04 -0300

    Inserir, Alterar e Deletar ParseObject

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 
-void main() {
+Future<void> main() async {
   runApp(MyApp());
+
+  //iniciar parse
+  await Parse().initialize(
+    '0xq3RbEIMesezRh1k93BjekPb2XWkdjDOyzBUJ9f',
+    'https://parseapi.back4app.com/',
+    clientKey: 'eGvnEtiRILccBxXWsaaojk2DZrGux8zm0wGKXfO8',
+    autoSendSessionId: true, //nossa identiuficação sempre estara aberta para não ter que informar cada vez que ue usar
+    debug: true, // para mostrar no terminal tudo que está acontecendo no parse.
+  );
+
+  //escrita de dados de teste
+  //objetos e dados são divididas em tabelas
+  //parse ParseObject é uma linha da tabela
+  /*final categoria = ParseObject('Categorias')
+    ..set<String>(
+      "titulo",
+      "Meias",
+    )
+    ..set<int>(
+      "posicao",
+      1,
+    );
+  final resposta = await categoria.save();
+  print(resposta.success);*/
+
+  //atualizar um ParseObject
+  /*final categoria = ParseObject("Categorias")
+    ..objectId = 'J7ghtGPV9u'
+    ..set<int>(
+      "posicao",
+      3,
+    );
+  final resposta = await categoria.save();
+  print(resposta.success);*/
+
+  //deletar um ParseObject
+  final categoria = ParseObject("Categorias")
+  ..objectId = 'J7ghtGPV9u';
+  categoria.delete();
 }
 
 class MyApp extends StatelessWidget {
