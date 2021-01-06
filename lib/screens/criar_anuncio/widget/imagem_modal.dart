c9ed624254518559c61dbb6fe7bebf3ee1492279
josimarminetefera <josimaminete@gmail.com>
2021-01-06 18:59:03 -0300

    Adicionado opções no modal para selecionar camera e galeria, tanto para ios quanto para android.

--- lib/screens/criar_anuncio/widget/imagem_modal.dart
+++ lib/screens/criar_anuncio/widget/imagem_modal.dart
@@ -1,25 +1,56 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ImagemModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("imagem_modal build()");
-    return BottomSheet(
-      onClosing: () {},
-      builder: (_) => Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch, //para tudo que está dentro ocupar tudo mesmo
-        children: [
-          FlatButton(
+    if ( Platform.isAndroid) {
+      return BottomSheet(
+        onClosing: () {}, //quando fecha o modal
+        builder: (_) => Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch, //para tudo que está dentro ocupar tudo mesmo
+          children: [
+            FlatButton(
+              onPressed: () {},
+              child: Text("Camera"),
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text("Galeria"),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return CupertinoActionSheet(
+        title: const Text("Selecionar foto para o anuncio"),
+        message: const Text("Escolha a origem da foto"),
+        cancelButton: CupertinoActionSheetAction(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text(
+            "Cancelar",
+            style: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+        ),
+        actions: [
+          CupertinoActionSheetAction(
             onPressed: () {},
             child: Text("Camera"),
           ),
-          FlatButton(
+          CupertinoActionSheetAction(
             onPressed: () {},
             child: Text("Galeria"),
           ),
         ],
-      ),
-    );
+      );
+    }
   }
 }
