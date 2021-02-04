3da07246820b2102cc4beb90f62b5d8c21bb8e8b
josimarminetefera <josimaminete@gmail.com>
2021-02-04 18:52:47 -0300

    Adicionado botão categoria dentro da tela de criar anuncio.

--- lib/screens/criar_anuncio/widget/categoria_field.dart
+++ lib/screens/criar_anuncio/widget/categoria_field.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:xlo_mobx/screens/categoria/categoria_screen.dart';
 import 'package:xlo_mobx/stores/criar_anuncio_store.dart';
 
 class CategoriaField extends StatelessWidget {
@@ -9,6 +10,28 @@ class CategoriaField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Observer(builder: (_) {
+      return ListTile(
+        title: Text(
+          "Categoria *",
+          style: TextStyle(
+            fontWeight: FontWeight.w800,
+            color: Colors.grey,
+            fontSize: 18,
+          ),
+        ),
+        trailing: Icon(Icons.keyboard_arrow_down),
+        onTap: () {
+          //este aqui vai abri uma janela sobre a tela com o botão voltar
+          showDialog(
+            context: context,
+            builder: (_) => CategoriaScreen(
+              mostrar_todas_categorias: false,
+              categoria_selecionada: criarAnuncioStore.categoria,
+            ),
+          );
+        },
+      );
+    });
   }
 }
